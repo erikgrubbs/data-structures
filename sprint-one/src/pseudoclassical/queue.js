e3ac6433fa2861ebaec294675c355b48c6540e18
Erik Grubbs <erikgrubbs@gmail.com>
2019-05-29 15:27:51 -0700

    completes the pseudoclassical implementations of stack and queue and passes spec

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.amount = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  for (var key in this.storage) {
+    this.storage[key] += 1;
+  }
+  this.storage[value] = 1;
+  this.amount++;
 };
 
 
+Queue.prototype.dequeue = function() {
+  var oldestEntryPosition = 0;
+  var oldestValue;
+  for (var key in this.storage) {
+    if (this.storage[key] > oldestEntryPosition) {
+      oldestEntryPosition = this.storage[key];
+      oldestValue = key;
+    }
+  }
+  delete this.storage[oldestValue];
+  this.amount > 0 ? this.amount-- : this.amount = this.amount;
+  return oldestValue;
+};
+
+Queue.prototype.size = function() {
+  return this.amount;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.amount = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.amount++;
+  this.storage[this.amount] = value;	
+};
 
+Stack.prototype.pop = function() {
+  var popped = this.storage[this.amount];
+  delete this.storage[this.amount];
+  if (this.amount > 0) {
+    this.amount--;
+  }
+  return popped;
+};
+
+Stack.prototype.size = function() {
+  return this.amount;
+};
