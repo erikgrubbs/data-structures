aa083fa68fcefa5b0f8e943936fb0b083e86d77c
Erik Grubbs <erikgrubbs@gmail.com>
2019-05-29 11:39:07 -0700

    Completes the stack.js implementation and gets all spec tests to pass.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,24 @@
 var Stack = function() {
   var someInstance = {};
-
-  // Use an object with numeric keys to store values
+  var size = 0;
   var storage = {};
 
-  // Implement the methods below
   someInstance.push = function(value) {
+    size++;
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    var popped = storage[size];
+    delete storage[size];
+    if(size > 0) {
+      size--;
+    }
+    return popped;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
