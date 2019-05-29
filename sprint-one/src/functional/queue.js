4041c3370a3fdac8361b94d2e856091106b66b1c
Erik Grubbs <erikgrubbs@gmail.com>
2019-05-29 11:58:35 -0700

    Completes the functional implementation of queue.js and passes all specs

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,32 @@
 var Queue = function() {
   var someInstance = {};
-
-  // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
+  var size = 0;
 
   someInstance.enqueue = function(value) {
+    for (var key in storage) {
+      storage[key] += 1;
+    }
+    storage[value] = 1;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    var oldestEntryPosition = 0;
+    var oldestValue;
+    for (var key in storage) {
+      if (storage[key] > oldestEntryPosition) {
+        oldestEntryPosition = storage[key];
+        oldestValue = key;
+      }
+    }
+    delete storage[oldestValue];
+    size > 0 ? size-- : size = size;
+    return oldestValue;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
