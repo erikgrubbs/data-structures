7f0e10043b601ce36663fbe8fd64e1e7ff7d3973
Erik Grubbs <erikgrubbs@gmail.com>
2019-05-30 11:46:09 -0700

    completes the given tree tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,32 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var tree = Tree(value);
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length !== 0) {
+    var isFound;
+    for (var i = 0; i < this.children.length; i++) {
+      if (isFound) {
+        break;
+      }
+      isFound = this.children[i].contains(target);
+    }
+    return isFound;
+  } 
+  return false;
 };
 
 
