4faac3b232e4f58bb5189bb2cd2cd54648bfbbf2
Erik Grubbs <erikgrubbs@gmail.com>
2019-05-29 19:30:15 -0700

    begins implementation of linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (list.head === null) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      var holder = list.head; 
+      while (holder.next) {
+        holder = holder.next;
+      }
+      holder.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var holder = list.head.value;
+    list.head = list.head.next;
+    return holder;
   };
 
   list.contains = function(target) {
+    var holder = list.head;
+    var doesContain = false;
+    while (holder.next) {
+      doesContain = (holder.value === target);
+      if (doesContain) {
+        return doesContain;
+      }
+      holder = holder.next;
+    }
+    return doesContain;
   };
 
   return list;
